2f564ee7cdf641902036a714335ea1bb81bfbd53
axixe <s1ngleez@yandex.ru>
2025-06-04 15:01:19 +0300

    feat(chatBlockMock): create new mock

---
+++ global/mockData/chatBlockMock.ts
@@ -0,0 +1,22 @@
+import type { ChatBlockUserData } from "~/global/types/ChatBlockUserData";
+
+export const chatBlockMock: ChatBlockUserData[] = [
+    {
+        avatar: { src: 'https://placehold.co/56x56' },
+        name: 'Alina Chernakova',
+        message: 'I gonna sleep. It’s so hard to do ...',
+        isRead: false,
+    },
+    {
+        avatar: { src: 'https://placehold.co/56x56' },
+        name: 'Alina Chernakova',
+        message: 'I gonna sleep. It’s so hard to do ...',
+        isRead: true,
+    },
+    {
+        avatar: { src: 'https://placehold.co/56x56' },
+        name: 'Alina Chernakova',
+        message: 'I gonna sleep. It’s so hard to do ...',
+        isRead: false,
+    },
+]
