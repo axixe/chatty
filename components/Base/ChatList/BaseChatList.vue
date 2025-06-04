727dded2f5a9bb1ca140cd4e7e9247eab096363e
axixe <s1ngleez@yandex.ru>
2025-06-04 15:02:29 +0300

    feat(BaseChatList): create new component

---
+++ components/Base/ChatList/BaseChatList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="chat-list">
+    <span class="chat-list__title">Chat list</span>
+
+    <div class="chat-list__list-wrapper">
+      <BaseChatBlock v-for="chatBlock in chats" :user-data="chatBlock" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { ChatBlockUserData } from "~/global/types/ChatBlockUserData";
+
+interface Props {
+  chats: ChatBlockUserData[]
+}
+
+defineProps<Props>()
+</script>
+
+<style scoped lang="scss">
+.chat-list {
+  display: flex;
+  flex-direction: column;
+  gap: $indent-l;
+
+  &__title {
+    color: $color-grey;
+  }
+
+  &__list-wrapper {
+    display: flex;
+    flex-direction: column;
+    gap: $indent-xs;
+  }
+}
+</style>
