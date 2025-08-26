874504809806100e39c940c4f4ab0b78c1006155
axixe <s1ngleez@yandex.ru>
2025-08-27 00:09:36 +0300

    feat(TheDialog): create component

---
+++ components/Dialog/TheDialog.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="dialog">
+    <DialogHeader
+      :avatar="{ src: 'https://placehold.co/50x50' }"
+      :username="'Alexander Chernakov'"
+      :status="'online'"
+    />
+
+    <DialogBody />
+
+    <ChatTextarea />
+  </div>
+</template>
+
+<script setup lang="ts">
+import DialogBody from '~/components/Dialog/DialogBody.vue'
+import ChatTextarea from '~/components/ui/ChatTextarea.vue'
+</script>
+
+<style scoped lang="scss">
+.dialog {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+}
+</style>
