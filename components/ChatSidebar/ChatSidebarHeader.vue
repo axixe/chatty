2fb3a4a33704133d04b9ffd752e88cb18d96bf48
axixe <s1ngleez@yandex.ru>
2025-08-27 13:37:04 +0300

    feat(ChatSidebarHeader): update component

--- components/ChatSidebar/ChatSidebarHeader.vue
+++ components/ChatSidebar/ChatSidebarHeader.vue
@@ -1,20 +1,17 @@
 <template>
   <div class="chat-sidebar-header">
+    <ButtonHamburger />
+
     <BaseImage
       :image="{ src: '/images/svg/logo.svg' }"
       :image-options="{ alt: 'logo', width: 122, height: 34 }"
-    />
-
-    <BaseImage
-      :image="avatar"
-      :image-options="{ alt: 'avatar', width: 50, height: 50 }"
-      class="chat-sidebar-header__avatar"
     />
   </div>
 </template>
 
 <script setup lang="ts">
 import type { Image } from '~/global/types/Image/Image'
+import ButtonHamburger from '~/components/ui/ButtonHamburger.vue'
 
 interface Props {
   avatar: Image
@@ -28,8 +25,8 @@ defineProps<Props>()
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: $indent-l;
-  border-bottom: 1px solid $color-base-light;
+  padding: 28px $indent-l;
+  border-bottom: 1px solid $color-base-medium;
 
   &__avatar {
     border-radius: 100%;
