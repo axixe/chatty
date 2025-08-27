352ec8db896d38af55d8bd6af5f5b348d4d36773
axixe <s1ngleez@yandex.ru>
2025-08-27 14:06:53 +0300

    feat(EmptyDialogBlock): create component

---
+++ components/ui/EmptyDialogBlock.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="empty-dialog-block h2">
+    <span>Your dialog</span>
+    <span class="empty-dialog-block__with-background">will be here</span>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.empty-dialog-block {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: $indent-s;
+  width: 100%;
+  height: 100vh;
+  pointer-events: none;
+
+  &__with-background {
+    padding: $indent-xs;
+    border-radius: 6px;
+    background: $color-base-medium;
+  }
+}
+</style>
