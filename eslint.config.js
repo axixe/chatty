c6d5b6b48ecc5a33c9c248e7f9f9c790e9362808
axixe <s1ngleez@yandex.ru>
2025-08-27 13:44:45 +0300

    feat(eslint.config): implement nuxt to eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -4,31 +4,35 @@ import typescriptParser from '@typescript-eslint/parser'
 import vueParser from 'vue-eslint-parser'
 import ts from 'typescript-eslint'
 import js from '@eslint/js'
+import { withNuxt } from './.nuxt/eslint.config.mjs'
 
-export default defineConfig([
-  js.configs.recommended,
-  ts.configs.recommended,
-  ...vue.configs['flat/recommended'],
-  {
-    ignores: ['node_modules/**', '.nuxt/**'],
-  },
-  {
-    name: 'chatty/recommended-rules',
-    files: ['**/*.ts', '**/*.vue'],
-    languageOptions: {
-      parser: vueParser,
-      parserOptions: {
-        parser: typescriptParser,
-        sourceType: 'module',
-        ecmaVersion: 'latest',
-        extraFileExtensions: ['.vue'],
+export default withNuxt(
+  defineConfig([
+    js.configs.recommended,
+    ts.configs.recommended,
+    ...vue.configs['flat/recommended'],
+    {
+      ignores: ['node_modules/**', '.nuxt/**'],
+    },
+    {
+      name: 'chatty/recommended-rules',
+      files: ['**/*.ts', '**/*.vue'],
+      languageOptions: {
+        parser: vueParser,
+        parserOptions: {
+          parser: typescriptParser,
+          sourceType: 'module',
+          ecmaVersion: 'latest',
+          extraFileExtensions: ['.vue'],
+        },
+      },
+      plugins: {
+        vue,
+      },
+      rules: {
+        'vue/html-self-closing': 'off',
+        'vue/multi-word-component-names': 'off',
       },
     },
-    plugins: {
-      vue,
-    },
-    rules: {
-      'vue/multi-word-component-names': 'off',
-    },
-  },
-])
+  ])
+)
