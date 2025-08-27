370e6389bdd4eb7429a0bdc389fc55394935e14a
axixe <s1ngleez@yandex.ru>
2025-08-27 14:07:33 +0300

    feat(index): implement dialog in index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,22 @@
 <template>
   <div class="home-page">
     <ChatSidebar />
+
+    <TheDialog v-if="isDialogOpened" />
+
+    <EmptyDialogBlock v-else />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TheDialog from '~/components/Dialog/TheDialog.vue'
+import EmptyDialogBlock from '~/components/ui/EmptyDialogBlock.vue'
 
-<style scoped lang="scss"></style>
+const isDialogOpened = ref(false)
+</script>
+
+<style scoped lang="scss">
+.home-page {
+  display: flex;
+}
+</style>
